ccf4343cd7337a37d1c8330ee7a500f868bb3547
Arron <beale86@msn.com>
2022-11-23 23:44:14 +0000

    Add base.css, add logo images and home image

--- teetime/urls.py
+++ teetime/urls.py
@@ -4,6 +4,8 @@
 # 3rd party:
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Internal:
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -12,4 +14,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
