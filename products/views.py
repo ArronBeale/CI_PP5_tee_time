5f43118eb45593bdb22db7bf6bcd007817aa34b1
Arron <beale86@msn.com>
2022-11-26 20:25:24 +0000

    Add layout to product_list.html

--- products/views.py
+++ products/views.py
@@ -12,9 +12,11 @@ def all_products(request):
     """ A view to show all products, sorting and search queries """
 
     products = Product.objects.all()
+    categories = Category.objects.all()
 
     context = {
         'products': products,
+        'categories': categories
     }
 
     return render(request, 'products/product_list.html', context)
