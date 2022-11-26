f66d62ebe2d39d4d5ad1aae600edd8b73645c12f
Arron <beale86@msn.com>
2022-11-26 17:58:52 +0000

    Add Product and Category  models to product app, import categories and products from fixtures

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,12 @@
+# Imports
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# 3rd party:
 from django.contrib import admin
 
-# Register your models here.
+# Internal
+from .models import Category, Product
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+admin.site.register(Product)
+admin.site.register(Category)
--- products/models.py
+++ products/models.py
@@ -1,3 +1,61 @@
+# Imports
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# 3rd Party
 from django.db import models
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(
+        max_length=254,
+        null=True,
+        blank=True
+        )
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
+class Product(models.Model):
+    code = models.CharField(
+        max_length=50,
+        null=True,
+        blank=True
+        )
+    brand = models.CharField(
+        max_length=50,
+        null=True,
+        blank=True
+        )
+    name = models.CharField(
+        max_length=254,
+        unique=True
+        )
+    description = models.TextField()
+    price = models.DecimalField(
+        max_digits=6,
+        decimal_places=2
+        )
+    category = models.ForeignKey(
+        'Category',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL
+        )
+    rating = models.DecimalField(
+        max_digits=6,
+        decimal_places=2,
+        null=True,
+        blank=True
+        )
+    image = models.ImageField(
+        null=True,
+        blank=True
+        )
+
+    def __str__(self):
+        return self.name
