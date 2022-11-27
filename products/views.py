872d3d53774455c73ee6a497e01c0900f0e035da
Arron <beale86@msn.com>
2022-11-27 14:10:36 +0000

    Add search ability to search bar

--- products/views.py
+++ products/views.py
@@ -2,7 +2,8 @@
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # 3rd Party
 from django.shortcuts import render, redirect, reverse, get_object_or_404
-from django.views.generic import ListView
+from django.contrib import messages
+from django.db.models import Q
 # Internal
 from .models import Product, Category
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -13,10 +14,23 @@ def all_products(request):
 
     products = Product.objects.all()
     categories = Category.objects.all()
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "Please input search criteria")
+                return redirect(reverse('products'))
+
+            queries = Q(name__icontains=query) | Q(
+                description__icontains=query)
+            products = products.filter(queries)
 
     context = {
         'products': products,
-        'categories': categories
+        'categories': categories,
+        'search_term': query
     }
 
     return render(request, 'products/product_list.html', context)
