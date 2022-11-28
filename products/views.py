7632421d2802b84caf66b622bfaf5c76a121b86d
Arron <beale86@msn.com>
2022-11-28 22:34:38 +0000

    Add sorting box to product list page

--- products/views.py
+++ products/views.py
@@ -36,7 +36,7 @@ def all_products(request):
 
     if request.GET:
         if 'category' in request.GET:
-            categories = request.GET['category']
+            categories = request.GET['category'].split(',')
             products = products.filter(category__name__in=categories)
             categories = Category.objects.filter(name__in=categories)
 
