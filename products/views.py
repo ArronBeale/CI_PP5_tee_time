9178c3a6ae32e8abec3a2db25dd89e48f80a6867
Arron <beale86@msn.com>
2022-11-28 19:50:23 +0000

    Add sorting by price, category, brand, rating to products page and product detail page

--- products/views.py
+++ products/views.py
@@ -16,10 +16,27 @@ def all_products(request):
     categories_list = Category.objects.all()
     query = None
     category = None
+    sort = None
+    direction = None
+
+    if request.GET:
+        if 'sort' in request.GET:
+            sort_key = request.GET['sort']
+            sort = sort_key
+            if sort_key == 'category':
+                sort_key = 'category__name'
+            if sort_key == 'name':
+                sort_key = 'lower_name'
+                products = products.annotate(lower_name=Lower('name'))
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sort_key = f'-{sort_key}'
+            products = products.order_by(sort_key)
 
     if request.GET:
         if 'category' in request.GET:
-            categories = request.GET['category'].split(',')
+            categories = request.GET['category']
             products = products.filter(category__name__in=categories)
             categories = Category.objects.filter(name__in=categories)
 
@@ -34,10 +51,13 @@ def all_products(request):
                 description__icontains=query)
             products = products.filter(queries)
 
+    current_sort = f'{sort}_{direction}'
+
     context = {
         'products': products,
         'categories_list': categories_list,
-        'search_term': query
+        'search_term': query,
+        'current_sort': current_sort
     }
 
     return render(request, 'products/product_list.html', context)
