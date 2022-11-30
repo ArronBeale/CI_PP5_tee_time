9e89584b9159cc2365494c3e2abe44e8184a5cdb
Arron <beale86@msn.com>
2022-11-30 18:59:35 +0000

    Add free delivery threshold, add context.py to basket app

---
+++ basket/contexts.py
@@ -0,0 +1,30 @@
+from decimal import Decimal
+from django.conf import settings
+
+
+def basket_contents(request):
+
+    basket_items = []
+    product_count = 0
+    total = 0
+
+    if total < settings.FREE_DELIVERY_THRESHOLD:
+        delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE/100)
+        free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
+    else:
+        delivery = 0
+        free_delivery_delta = 0
+
+    grand_total = total + delivery
+
+    context = {
+        'basket_items': basket_items,
+        'product_count': product_count,
+        'total': total,
+        'delivery': delivery,
+        'free_delivery_delta': free_delivery_delta,
+        'free_delivery_threshold': settings.FREE_DELIVERY_THRESHOLD,
+        'grand_total': grand_total,
+    }
+
+    return context
--- teetime/settings.py
+++ teetime/settings.py
@@ -81,6 +81,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',  # required
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'basket.contexts.basket_contents',
             ],
         },
     },
@@ -183,3 +184,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+FREE_DELIVERY_THRESHOLD = 50
+STANDARD_DELIVERY_PERCENTAGE = 10
