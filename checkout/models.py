30bf9c6fda06a9e337583b2fc5cdaf69bafbc153
Arron <beale86@msn.com>
2022-12-05 18:08:54 +0000

    Add OrderLineItem class to checkout app models

--- checkout/models.py
+++ checkout/models.py
@@ -13,85 +13,131 @@ from products.models import Product
 
 
 class Order(models.Model):
-    class Order(models.Model):
-        order_number = models.CharField(
-            max_length=35,
-            null=False,
-            editable=False
+    order_number = models.CharField(
+        max_length=35,
+        null=False,
+        editable=False
+    )
+    full_name = models.CharField(
+        max_length=55,
+        null=False,
+        blank=False
+    )
+    email = models.EmailField(
+        max_length=254,
+        null=False,
+        blank=False
+    )
+    phone_number = models.CharField(
+        max_length=20,
+        null=False,
+        blank=False
+    )
+    address1 = models.CharField(
+        max_length=85,
+        null=False,
+        blank=False
+    )
+    address2 = models.CharField(
+        max_length=85,
+        null=True,
+        blank=True
+    )
+    town_or_city = models.CharField(
+        max_length=45,
+        null=False,
+        blank=False
+    )
+    postcode = models.CharField(
+        max_length=25,
+        null=True,
+        blank=True
+    )
+    county = models.CharField(
+        max_length=85,
+        null=True,
+        blank=True
+    )
+    country = CountryField(
+        blank_label='Country *',
+        null=False,
+        blank=False
+    )
+    date = models.DateTimeField(
+        auto_now_add=True
         )
-        full_name = models.CharField(
-            max_length=55,
-            null=False,
-            blank=False
+    delivery_cost = models.DecimalField(
+        max_digits=6,
+        decimal_places=2,
+        null=False,
+        default=0
+    )
+    order_total = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        null=False,
+        default=0
+    )
+    grand_total = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        null=False,
+        default=0
+    )
+    original_bag = models.TextField(
+        null=False,
+        blank=False,
+        default=''
+    )
+    stripe_pid = models.CharField(
+        max_length=254,
+        null=False,
+        blank=False,
+        default='')
+
+
+class OrderLineItem(models.Model):
+    """
+    A model for a website order line item
+    """
+    order = models.ForeignKey(
+        Order,
+        null=False,
+        blank=False,
+        on_delete=models.CASCADE,
+        related_name='lineitems'
         )
-        email = models.EmailField(
-            max_length=254,
-            null=False,
-            blank=False
+    product = models.ForeignKey(
+        Product,
+        null=False,
+        blank=False,
+        on_delete=models.CASCADE
         )
-        phone_number = models.CharField(
-            max_length=20,
-            null=False,
-            blank=False
+    product_size = models.CharField(
+        max_length=2,
+        null=True,
+        blank=True
         )
-        address1 = models.CharField(
-            max_length=85,
-            null=False,
-            blank=False
+    quantity = models.IntegerField(
+        null=False,
+        blank=False,
+        default=0
         )
-        address2 = models.CharField(
-            max_length=85,
-            null=True,
-            blank=True
+    lineitem_total = models.DecimalField(
+        max_digits=6,
+        decimal_places=2,
+        null=False,
+        blank=False,
+        editable=False
         )
-        town_or_city = models.CharField(
-            max_length=45,
-            null=False,
-            blank=False
-        )
-        postcode = models.CharField(
-            max_length=25,
-            null=True,
-            blank=True
-        )
-        county = models.CharField(
-            max_length=85,
-            null=True,
-            blank=True
-        )
-        country = CountryField(
-            blank_label='Country *',
-            null=False,
-            blank=False
-        )
-        date = models.DateTimeField(
-            auto_now_add=True
-            )
-        delivery_cost = models.DecimalField(
-            max_digits=6,
-            decimal_places=2,
-            null=False,
-            default=0
-        )
-        order_total = models.DecimalField(
-            max_digits=10,
-            decimal_places=2,
-            null=False,
-            default=0
-        )
-        grand_total = models.DecimalField(
-            max_digits=10,
-            decimal_places=2,
-            null=False,
-            default=0
-        )
-        original_bag = models.TextField(
-            null=False,
-            blank=False,
-            default=''
-        )
-        stripe_pid = models.CharField(
-            max_length=254,
-            null=False,
-            blank=False,
-            default='')
+
+    def save(self, *args, **kwargs):
+        """
+        Override the original save method to set the lineitem total
+        and update the order total.
+        """
+        self.lineitem_total = self.product.price * self.quantity
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f'SKU {self.product.sku} on order {self.order.order_number}'
