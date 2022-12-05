c68d90b9a5ea8da2fe4c558c5e412824ece19e01
Arron <beale86@msn.com>
2022-12-05 19:02:39 +0000

    Add Order model, OrderLineItem model to checkout app, register in admin.py

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,58 @@
+# Imports
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# 3rd Party
 from django.contrib import admin
+from django.contrib import admin
+# Internal
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+from .models import Order, OrderLineItem
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-# Register your models here.
+
+class OrderLineItemAdminInline(admin.TabularInline):
+    model = OrderLineItem
+    readonly_fields = ('lineitem_total',)
+
+
+class OrderAdmin(admin.ModelAdmin):
+    inlines = (OrderLineItemAdminInline,)
+
+    readonly_fields = (
+        'order_number',
+        'date',
+        'delivery_cost',
+        'order_total',
+        'grand_total',
+        'original_basket',
+        'stripe_pid',
+    )
+    fields = (
+        'order_number',
+        'date',
+        'full_name',
+        'email',
+        'phone_number',
+        'address1',
+        'address2',
+        'town_or_city',
+        'postcode',
+        'county',
+        'country',
+        'delivery_cost',
+        'order_total',
+        'grand_total',
+        'original_basket',
+        'stripe_pid',
+    )
+    list_display = (
+        'order_number',
+        'date',
+        'full_name',
+        'delivery_cost',
+        'order_total',
+        'grand_total',
+    )
+    ordering = ('-date',)
+
+
+admin.site.register(Order, OrderAdmin)
--- checkout/models.py
+++ checkout/models.py
@@ -16,55 +16,66 @@ class Order(models.Model):
     order_number = models.CharField(
         max_length=35,
         null=False,
-        editable=False
+        editable=False,
+        default=''
     )
     full_name = models.CharField(
         max_length=55,
         null=False,
-        blank=False
+        blank=False,
+        default=''
     )
     email = models.EmailField(
         max_length=254,
         null=False,
-        blank=False
+        blank=False,
+        default=''
     )
     phone_number = models.CharField(
         max_length=20,
         null=False,
-        blank=False
+        blank=False,
+        default=''
     )
     address1 = models.CharField(
         max_length=85,
         null=False,
-        blank=False
+        blank=False,
+        default=''
     )
     address2 = models.CharField(
         max_length=85,
         null=True,
-        blank=True
+        blank=True,
+        default=''
     )
     town_or_city = models.CharField(
         max_length=45,
         null=False,
-        blank=False
+        blank=False,
+        default=''
     )
     postcode = models.CharField(
         max_length=25,
         null=True,
-        blank=True
+        blank=True,
+        default=''
     )
     county = models.CharField(
         max_length=85,
         null=True,
-        blank=True
+        blank=True,
+        default=''
     )
     country = CountryField(
         blank_label='Country *',
         null=False,
-        blank=False
+        blank=False,
+        default=''
     )
     date = models.DateTimeField(
-        auto_now_add=True
+        auto_now_add=True,
+        null=True
         )
     delivery_cost = models.DecimalField(
         max_digits=6,
@@ -84,7 +95,7 @@ class Order(models.Model):
         null=False,
         default=0
     )
-    original_bag = models.TextField(
+    original_basket = models.TextField(
         null=False,
         blank=False,
         default=''
@@ -95,10 +106,40 @@ class Order(models.Model):
         blank=False,
         default='')
 
+    def _generate_order_number(self):
+        """
+        Generate a random, unique order number using UUID
+        """
+        return uuid.uuid4().hex.upper()
+
+    def update_total(self):
+        """
+        Update grand total each time a line item is added
+        """
+        self.order_total = self.lineitems.aggregate(
+            Sum('lineitem_total'))['lineitem_total__sum'] or 0
+        if self.order_total < settings.FREE_DELIVERY_THRESHOLD:
+            self.delivery_cost = self.order_total * settings.STANDARD_DELIVERY_PERCENTAGE / 100
+        else:
+            self.delivery_cost = 0
+        self.grand_total = self.order_total + self.delivery_cost
+        self.save()
+
+    def save(self, *args, **kwargs):
+        """
+        Override the original save method to set the order number
+        """
+        if not self.order_number:
+            self.order_number = self._generate_order_number()
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.order_number
+
 
 class OrderLineItem(models.Model):
     """
-    A model for a website order line item
+    A model for a web order line item
     """
     order = models.ForeignKey(
         Order,
