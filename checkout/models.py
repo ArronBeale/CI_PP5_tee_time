06de9edbb93d3b51a8dd4a64cbcec9399a1335eb
Arron <beale86@msn.com>
2022-12-06 20:58:47 +0000

    Adjust checkout_success.htnl layout

--- checkout/models.py
+++ checkout/models.py
@@ -181,4 +181,4 @@ class OrderLineItem(models.Model):
         super().save(*args, **kwargs)
 
     def __str__(self):
-        return f'SKU {self.product.sku} on order {self.order.order_number}'
+        return f'SKU {self.product.code} on order {self.order.order_number}'
--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -73,8 +73,8 @@ form.addEventListener('submit', function(ev) {
                     phone: $.trim(form.phone_number.value),
                     email: $.trim(form.email.value),
                     address:{
-                        line1: $.trim(form.street_address1.value),
-                        line2: $.trim(form.street_address2.value),
+                        line1: $.trim(form.address1.value),
+                        line2: $.trim(form.address2.value),
                         city: $.trim(form.town_or_city.value),
                         country: $.trim(form.country.value),
                         state: $.trim(form.county.value),
@@ -85,8 +85,8 @@ form.addEventListener('submit', function(ev) {
                 name: $.trim(form.full_name.value),
                 phone: $.trim(form.phone_number.value),
                 address: {
-                    line1: $.trim(form.street_address1.value),
-                    line2: $.trim(form.street_address2.value),
+                    line1: $.trim(form.address1.value),
+                    line2: $.trim(form.address2.value),
                     city: $.trim(form.town_or_city.value),
                     country: $.trim(form.country.value),
                     postal_code: $.trim(form.postcode.value),
