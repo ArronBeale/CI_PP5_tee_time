898bb262e0044a345b853df135ee668f10f5fc45
Arron <beale86@msn.com>
2022-12-10 15:40:26 +0000

    Fix address1 & address2 in forms.py of profiles app

--- profiles/forms.py
+++ profiles/forms.py
@@ -22,8 +22,8 @@ class UserProfileForm(forms.ModelForm):
             'default_phone_number': 'Phone Number',
             'default_postcode': 'Postal Code',
             'default_town_or_city': 'Town or City',
-            'default_street_address1': 'Street Address 1',
-            'default_street_address2': 'Street Address 2',
+            'default_address1': 'Street Address 1',
+            'default_address2': 'Street Address 2',
             'default_county': 'County, State or Locality',
         }
 
