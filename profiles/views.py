39d7a420c5b6cc60bc1f2a27698e800352c62954
Arron <beale86@msn.com>
2022-12-10 14:15:03 +0000

    Add views and urls to profiles app

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,69 @@
+# Imports
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# 3rd Party
 from django.shortcuts import render
+# Internal
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+from .models import UserProfile
+from .forms import UserProfileForm
+from checkout.models import Order
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-# Create your views here.
+
+@login_required
+def profile(request):
+    """
+    Display user profile
+    """
+    profile = get_object_or_404(
+        UserProfile,
+        user=request.user
+        )
+
+    if request.method == 'POST':
+        form = UserProfileForm(
+            request.POST,
+            instance=profile
+            )
+        if form.is_valid():
+            form.save()
+            messages.success(
+                request, 'Profile updated successfully'
+                )
+        else:
+            messages.error(
+                request, 'Update failed. Please check the form is valid.'
+                )
+    else:
+        form = UserProfileForm(instance=profile)
+
+    orders = profile.orders.all()
+
+    template = 'profiles/profile.html'
+    context = {
+        'form': form,
+        'orders': orders,
+        'on_profile_page': True,
+    }
+
+    return render(request, template, context)
+
+
+def order_history(request, order_number):
+    order = get_object_or_404(
+        Order,
+        order_number=order_number
+        )
+
+    messages.info(request, (
+        f'This is a previous confirmation for order number {order_number}. '
+        'Confirmation email sent on the order date.'
+    ))
+
+    template = 'checkout/checkout_success.html'
+    context = {
+        'order': order,
+        'from_profile': True,
+    }
+
+    return render(request, template, context)
