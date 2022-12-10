8b9e9d0adcc9dfa8101251f5c3ef33fbcbdeb5c0
Arron <beale86@msn.com>
2022-12-10 14:58:40 +0000

    Adjust allauth login template

--- profiles/views.py
+++ profiles/views.py
@@ -1,7 +1,9 @@
 # Imports
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # 3rd Party
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 # Internal
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 from .models import UserProfile
