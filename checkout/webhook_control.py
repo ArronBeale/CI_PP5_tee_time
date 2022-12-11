cec4b289ff443236de133234c6729dd12366236f
Arron <beale86@msn.com>
2022-12-11 14:47:28 +0000

    Add user profile to webhook control

--- checkout/webhook_control.py
+++ checkout/webhook_control.py
@@ -11,6 +11,7 @@ import time
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 from .models import Order, OrderLineItem
 from products.models import Product
+from profiles.models import UserProfile
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 
@@ -77,19 +78,19 @@ class StripeWebHook_Handler:
                 shipping_details.address[field] = None
 
         # Update profile info if save_info
-        # profile = None
-        # username = intent.metadata.username
-        # if username != 'AnonymousUser':
-        #     profile = UserProfile.objects.get(user__username=username)
-        #     if save_info:
-        #         profile.default_phone_number = shipping_details.phone
-        #         profile.default_country = shipping_details.address.country
-        #         profile.default_postcode = shipping_details.address.postal_code
-        #         profile.default_town_or_city = shipping_details.address.city
-        #         profile.default_address1 = shipping_details.address.line1
-        #         profile.default_address2 = shipping_details.address.line2
-        #         profile.default_county = shipping_details.address.state
-        #         profile.save()
+        profile = None
+        username = intent.metadata.username
+        if username != 'AnonymousUser':
+            profile = UserProfile.objects.get(user__username=username)
+            if save_info:
+                profile.default_phone_number = shipping_details.phone
+                profile.default_country = shipping_details.address.country
+                profile.default_postcode = shipping_details.address.postal_code
+                profile.default_town_or_city = shipping_details.address.city
+                profile.default_address1 = shipping_details.address.line1
+                profile.default_address2 = shipping_details.address.line2
+                profile.default_county = shipping_details.address.state
+                profile.save()
 
         order_exists = False
         attempt = 1
