5b7001cd9f43dda6b3b084c152e563ab802b3fd0
Arron <beale86@msn.com>
2022-12-13 18:52:24 +0000

    Add localhost to allowed host in settings.py

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn teetime.wsgi:application
--- teetime/settings.py
+++ teetime/settings.py
@@ -34,7 +34,7 @@ DEBUG = development
 if development:
     ALLOWED_HOSTS = ['localhost', '127.0.0.1', '127.0.0.1:8000']
 else:
-    ALLOWED_HOSTS = ["https://ci-pp5-teetime.herokuapp.com/"]
+    ALLOWED_HOSTS = ['https://ci-pp5-teetime.herokuapp.com/', 'localhost']
 
 
 # Application definition
