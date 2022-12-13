430f4307a213c385a2dc7c6bec32dc9837d07897
Arron <beale86@msn.com>
2022-12-13 20:32:28 +0000

    Add AWS s3 settings to settings.py for static files, add custom_storages.py for aws static files bucket

---
+++ custom_storages.py
@@ -0,0 +1,16 @@
+# Imports
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# 3rd Party
+from django.conf import settings
+from storages.backends.s3boto3 import S3Boto3Storage
+# Internal
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+class StaticStorage(S3Boto3Storage):
+    location = settings.STATICFILES_LOCATION
+
+
+class MediaStorage(S3Boto3Storage):
+    location = settings.MEDIAFILES_LOCATION
