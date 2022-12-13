a7cf64dd1a3f3e87532b29775d72171e554863b0
Arron <beale86@msn.com>
2022-12-13 18:40:36 +0000

    Adjust database settings in settings.py for deployment

--- teetime/settings.py
+++ teetime/settings.py
@@ -136,24 +136,24 @@ WSGI_APPLICATION = 'teetime.wsgi.application'
 #     }
 # }
 
-DATABASES = {
-    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
-}
+# DATABASES = {
+#     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+# }
 
-# if 'DATABASE_URL' in os.environ:
+if 'DATABASE_URL' in os.environ:
 
-#     DATABASES = {
-#         'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
-#     }
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+    }
 
-# else:
+else:
 
-#     DATABASES = {
-#         'default': {
-#             'ENGINE': 'django.db.backends.sqlite3',
-#             'NAME': BASE_DIR / 'db.sqlite3',
-#         }
-#     }
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
