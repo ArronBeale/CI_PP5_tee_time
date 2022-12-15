80460eae294bd443deb397a82f47bb466dfa9e9d
Arron <beale86@msn.com>
2022-12-15 20:34:28 +0000

    webhook test

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -24,25 +24,25 @@ class StripeWebHook_Handler:
     def __init__(self, request):
         self.request = request
 
-    # def _send_confirmation_email(self, order):
-    #     """
-    #     Send confirmation email to user
-    #     """
-    #     customer_email = order.email
-    #     subject = render_to_string(
-    #         'checkout/confirmation_emails/confirmation_email_subject.txt',
+    def _send_confirmation_email(self, order):
+        """
+        Send confirmation email to user
+        """
+        customer_email = order.email
+        subject = render_to_string(
+            'checkout/confirmation_emails/confirmation_email_subject.txt',
 
-    #         {'order': order})
-    #     body = render_to_string(
-    #         'checkout/confirmation_emails/confirmation_email_body.txt',
-    #         {'order': order, 'contact_email': settings.DEFAULT_FROM_EMAIL})
+            {'order': order})
+        body = render_to_string(
+            'checkout/confirmation_emails/confirmation_email_body.txt',
+            {'order': order, 'contact_email': settings.DEFAULT_FROM_EMAIL})
 
-    #     send_mail(
-    #         subject,
-    #         body,
-    #         settings.DEFAULT_FROM_EMAIL,
-    #         [customer_email]
-    #     )
+        send_mail(
+            subject,
+            body,
+            settings.DEFAULT_FROM_EMAIL,
+            [customer_email]
+        )
 
     def handle_event(self, event):
         """
@@ -58,7 +58,7 @@ class StripeWebHook_Handler:
         """
         intent = event.data.object
         pid = intent.id
-        bag = intent.metadata.basket
+        bag = intent.metadata.bag
         save_info = intent.metadata.save_info
 
         # Get the Charge object
@@ -84,12 +84,12 @@ class StripeWebHook_Handler:
             profile = UserProfile.objects.get(user__username=username)
             if save_info:
                 profile.default_phone_number = shipping_details.phone
-                profile.default_country = shipping_details.address.country
-                profile.default_postcode = shipping_details.address.postal_code
-                profile.default_town_or_city = shipping_details.address.city
                 profile.default_address1 = shipping_details.address.line1
                 profile.default_address2 = shipping_details.address.line2
+                profile.default_town_or_city = shipping_details.address.city
                 profile.default_county = shipping_details.address.state
+                profile.default_postcode = shipping_details.address.postal_code
+                profile.default_country = shipping_details.address.country
                 profile.save()
 
         order_exists = False
@@ -101,12 +101,12 @@ class StripeWebHook_Handler:
                     full_name__iexact=shipping_details.name,
                     email__iexact=billing_details.email,
                     phone_number__iexact=shipping_details.phone,
-                    country__iexact=shipping_details.address.country,
-                    postcode__iexact=shipping_details.address.postal_code,
-                    town_or_city__iexact=shipping_details.address.city,
                     address1__iexact=shipping_details.address.line1,
                     address2__iexact=shipping_details.address.line2,
+                    town_or_city__iexact=shipping_details.address.city,
+                    postcode__iexact=shipping_details.address.postal_code,
                     county__iexact=shipping_details.address.state,
+                    country__iexact=shipping_details.address.country,
                     grand_total=grand_total,
                     original_basket=basket,
                     stripe_pid=pid,
@@ -133,16 +133,16 @@ class StripeWebHook_Handler:
                     user_profile=profile,
                     email=billing_details.email,
                     phone_number=shipping_details.phone,
-                    country=shipping_details.address.country,
-                    postcode=shipping_details.address.postal_code,
-                    town_or_city=shipping_details.address.city,
                     address1=shipping_details.address.line1,
                     address2=shipping_details.address.line2,
+                    town_or_city=shipping_details.address.city,
+                    postcode=shipping_details.address.postal_code,
                     county=shipping_details.address.state,
+                    country=shipping_details.address.country,
                     original_basket=basket,
                     stripe_pid=pid,
                 )
-                for item_id, item_data in json.loads(basket).items():
+                for item_id, item_data in json.loads(bag).items():
                     product = Product.objects.get(id=item_id)
                     if isinstance(item_data, int):
                         order_line_item = OrderLineItem(
