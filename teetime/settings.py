c7fa144ba98f1f59a63273b03962daf03e513b7c
Arron <beale86@msn.com>
2022-12-17 17:22:17 +0000

    Email test

--- teetime/settings.py
+++ teetime/settings.py
@@ -238,7 +238,7 @@ STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 
 if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-    DEFAULT_FROM_EMAIL = 'info@teetime.com'
+    DEFAULT_FROM_EMAIL = 'teetime@example.com'
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
