8c53f3ea35d39faa37d66fbc9f20cee3a7b5a2af
Arron <beale86@msn.com>
2023-01-03 20:06:02 +0000

    fix missing semicolon in stripe_elements.js, remove test.py as it is unused, add comments to base, tidy line too long in urls.py

--- products/widgets.py
+++ products/widgets.py
@@ -12,4 +12,6 @@ class CustClearableFileInput(ClearableFileInput):
     clear_checkbox_label = _('Remove')
     initial_text = _('Current_image')
     input_text = _('')
-    template_name = 'products/custom_widget_templates/cust_clearable_file_input.html'
+    template_name = (
+        'products/custom_widget_templates/cust_clearable_file_input.html'
+        )
